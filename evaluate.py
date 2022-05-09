3808c3061650e3e421646c64621d0346d600ffbf
eric-austin <eric.james.austin@gmail.com>
2022-05-09 17:03:43 -0600

    eval wt cpm

--- evaluate.py
+++ evaluate.py
@@ -15,17 +15,17 @@ def main():
     phrase = "npmi"
     phrase_threshold = "0.35"
 
-    with open("./bbc2_master_object.obj", "rb") as f:
+    with open("./ng1_master_object.obj", "rb") as f:
         master_object = pickle.load(f)
 
-    # ng_dict = master_object["ng_dict"]
+    ng_dict = master_object["ng_dict"]
     # rt_dict = master_object["rt_dict"]
-    bbc_dict = master_object["bbc_dict"]
+    # bbc_dict = master_object["bbc_dict"]
 
-    f = open("bbc2_results.csv", "a")
+    f = open("ng1_results_wtleiden.csv", "a")
     # want to go through each network, find the associated siwo communities and
     # mine the leiden communities, then evaluate all permutations
-    for network in os.listdir("./bbc2_networks"):
+    for network in os.listdir("./ng1_networks"):
         t0 = time()
         # f = open(f"ng1_results_{network}.csv", "a")
         # break up filename to grab params for network generation
@@ -36,49 +36,53 @@ def main():
         weight_threshold = details[3][:-4]
 
         # each network will have two associated siwo community partitions
-        siwo_a = community_utils.read_siwo_comms(f"./bbc2_siwo_comms/a_{network}")
-        siwo_g = community_utils.read_siwo_comms(f"./bbc2_siwo_comms/g_{network}")
-        # filter small comms
-        siwo_a = [comm for comm in siwo_a if len(comm) > 2]
-        siwo_g = [comm for comm in siwo_g if len(comm) > 2]
+        # siwo_a = community_utils.read_siwo_comms(f"./bbc2_siwo_comms/a_{network}")
+        # siwo_g = community_utils.read_siwo_comms(f"./bbc2_siwo_comms/g_{network}")
+        # # filter small comms
+        # siwo_a = [comm for comm in siwo_a if len(comm) > 2]
+        # siwo_g = [comm for comm in siwo_g if len(comm) > 2]
 
         # load network
-        nx_g = nx.read_weighted_edgelist(f"./bbc2_networks/{network}")
+        nx_g = nx.read_weighted_edgelist(f"./ng1_networks/{network}")
         ig_g = ig.Graph.from_networkx(nx_g)
 
         # use different resolution parameters for leiden
+        walktrap4 = ig_g.community_walktrap(weights='weight').as_clustering()
+        walktrap10 = ig_g.community_walktrap(weights='weight', steps=10).as_clustering()
         leiden_1 = ig_g.community_leiden(weights='weight', 
-                                        objective_function='modularity',
+                                        objective_function='CPM',
                                         resolution_parameter=1)
-        leiden_15 = ig_g.community_leiden(weights='weight', 
-                                        objective_function='modularity',
-                                        resolution_parameter=1.5)
-        leiden_2 = ig_g.community_leiden(weights='weight', 
-                                        objective_function='modularity',
-                                        resolution_parameter=2)
+        leiden_50 = ig_g.community_leiden(weights='weight', 
+                                        objective_function='CPM',
+                                        resolution_parameter=0.5)
         leiden_25 = ig_g.community_leiden(weights='weight', 
-                                        objective_function='modularity',
-                                        resolution_parameter=2.5)
+                                        objective_function='CPM',
+                                        resolution_parameter=0.25)
+        leiden_10 = ig_g.community_leiden(weights='weight', 
+                                        objective_function='CPM',
+                                        resolution_parameter=0.1)
         # filter small comms
+        walktrap4 = [[int(ig_g.vs[node]["_nx_name"]) for node in comm] for comm in walktrap4 if len(comm) > 2]
+        walktrap10 = [[int(ig_g.vs[node]["_nx_name"]) for node in comm] for comm in walktrap10 if len(comm) > 2]
         leiden_1 = [[int(ig_g.vs[node]["_nx_name"]) for node in comm] for comm in leiden_1 if len(comm) > 2]
-        leiden_15 = [[int(ig_g.vs[node]["_nx_name"]) for node in comm] for comm in leiden_15 if len(comm) > 2]
-        leiden_2 = [[int(ig_g.vs[node]["_nx_name"]) for node in comm] for comm in leiden_2 if len(comm) > 2]
+        leiden_50 = [[int(ig_g.vs[node]["_nx_name"]) for node in comm] for comm in leiden_50 if len(comm) > 2]
         leiden_25 = [[int(ig_g.vs[node]["_nx_name"]) for node in comm] for comm in leiden_25 if len(comm) > 2]
+        leiden_10 = [[int(ig_g.vs[node]["_nx_name"]) for node in comm] for comm in leiden_10 if len(comm) > 2]
 
         alg_params = [
-            ("siwo,a", siwo_a),
-            ("siwo,g", siwo_g),
+            ("walktrap,4", walktrap4),
+            ("walktrap,10", walktrap10),
             ("leiden,1.0", leiden_1),
-            ("leiden,1.5", leiden_15),
-            ("leiden,2.0", leiden_2),
-            ("leiden,2.5", leiden_25)
+            ("leiden,0.50", leiden_50),
+            ("leiden,0.25", leiden_25),
+            ("leiden,0.10", leiden_10)
         ]
         
         # now we have 2 siwo partitions and 4 leiden partitions to evaluate
         # will evaluate each with different term ranking functions
         for alg_param, partition in alg_params:
             # need the right dictionary
-            dictionary = bbc_dict
+            dictionary = ng_dict
 
 
             # sort by degree
